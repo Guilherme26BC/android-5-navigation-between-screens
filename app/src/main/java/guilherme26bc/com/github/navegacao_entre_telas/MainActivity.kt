c52bc396e693530dde006c28231ea68453a75a68
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-06 21:29:46 -0300

    first commit

---
+++ app/src/main/java/guilherme26bc/com/github/navegacao_entre_telas/MainActivity.kt
@@ -0,0 +1,47 @@
+package guilherme26bc.com.github.navegacao_entre_telas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import guilherme26bc.com.github.navegacao_entre_telas.ui.theme.Navegacao_entre_telasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Navegacao_entre_telasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Navegacao_entre_telasTheme {
+        Greeting("Android")
+    }
+}
